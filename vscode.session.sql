332ef14f7437d217d84774ef75418b9c163162c8
powerm9 <powerm9@tcd.ie>
2024-11-07 15:58:12 +0000

    to desktop

---
+++ vscode.session.sql
@@ -0,0 +1,16 @@
+
+
+-- INSERT INTO unit (uname, unumber, uhq, leader_sn, leader_startdate)
+-- VALUES
+-- ('Alpha Unit', 101, 'Barneystown', 2411, '2015-06-01');
+-- ('Bravo Unit', 102, 'Darndale', 2412, '2016-07-15'),
+-- ('Charlie Unit', 103, 'Pearse st', 2413, '2017-08-20'),
+-- ('Delta Unit', 104, 'Naas', 2414, '2018-09-10');
+
+-- INSERT INTO soldier (fname, lname, service_num, sex, curr_rank, salary, DOB, address, wounds, Unum)
+-- VALUES
+-- ('John', 'Rambo', 2411, 'Male', 'Private', 35000, '1990-05-15', '123 Hunter Sraid', NULL  , NULL);
+-- ('Jane', 'Smith', 2412 , 'Female', 'Lieutenant', 45000, '1985-08-25', '456 Lemon sticks avenue', 'Broken arm', 102),
+-- ('Alex', 'Musk', 2411, 'Male', 'Private', 35000, '1990-05-15', '123 In aice leis na tra', NULL, 103),
+-- ('Bruno', 'Lars', 2412 , 'Female', 'Sergeant', 45000, '1985-08-25', '918 trinity St', NULL, 104);
+
